26e21d7ab1713422b6e3b6b1202506e474fa4c09
Santhoshpodilibot <95113548+Santhoshpodilibot@users.noreply.github.com>
2022-02-15 14:01:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ UNSCREEN_API = os.environ.get("UNSCREEN_API", "")
 Bot = Client(
     "Remove Background Bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
+    api_id=os.environ.get("API_ID"),
     api_hash=os.environ.get("API_HASH")
 )
 
